dd7682847e4acebb960cf608f21ec25460c62003
JEAXXXN <125298697+JEAXXXN@users.noreply.github.com>
2023-03-06 13:19:19 -0500

    Add files via upload

---
+++ Estructuras de datos/nuevoo.js
@@ -0,0 +1,30 @@
+//Declarar variables
+var termino1 =5;
+var termino2 = 89;
+//sumar variables
+var suma = termino1 + termino2;
+console.log (suma)
+
+//Declarar variables (cadenas)
+var cadena1 =1;
+var cadena2= 2;
+//Sumar variables (cadenas)
+var suma = cadena1 + cadena2;
+console.log (suma)
+
+//Sumar variable1 con cadena1
+var suma = cadena1 + termino1;
+console.log (suma)
+
+//Declarar variable booleana
+var nuevo = termino1<10;
+//cONFIRMAR VARIABLE
+console.log (nuevo)
+
+//Promedio
+var nota1=2.0;
+var nota2=2.7;
+var nota3=5.0;
+var nota4=4.7;
+
+var suma2 (nota1+ nota2+ nota3+ nota4)/4;
